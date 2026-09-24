ffed0edfa61520e4aabdb8227fc15f8cd96d870b
Geoh8747 <geovanna.vasconcelos@estudante.ifms.edu.br>
2026-09-24 16:00:27 +0000

    exercicio 2

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).send('API de tarefas no Ar')
 });
+
+const tarefas= 
+[
+    {
+        id: 1,
+        titulo: 'Comprar alimentação',
+        concluida: true
+    },
+    {
+        id: 2,
+        titulo: 'Fazer prova',
+        concluida: true
+    },
+    {
+        id: 3,
+        titulo: 'Lavar a casa',
+        concluida: false
+    }
+];
+
+app.get('/tarefas', (req,res) => {
+    res.status(200).json(tarefas)
+});
